11c085429d394424b490417391a575715cf788a0
Ivan Lit <litvin.ivan.ak@gmail.com>
2020-11-08 22:40:21 +0200

    войти и зарегистрироваться

--- dolly/client/src/pages/SingIn.tsx
+++ dolly/client/src/pages/SingIn.tsx
@@ -1,99 +1,157 @@
-import React from 'react'
-import {Button, makeStyles, Typography} from "@material-ui/core";
+import React from 'react';
+import {makeStyles, Typography, Button} from '@material-ui/core';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
-import SearchIcon from '@material-ui/icons/Search';
-import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
-import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
-//Страница входа
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import {ModalBlock} from '../components/ModalBlock';
 
-const useStyles = makeStyles((theme) => ({
+export const useStylesSignIn = makeStyles((theme) => ({
     wrapper: {
         display: 'flex',
-        height: '100vh'
-    },
-    colorSide: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        height: '100vh',
         backgroundColor: '#71C9F8',
-        flex: '0 0 50%',
         overflow: 'hidden',
-        position: 'relative'
     },
-    colorSideListInfo: {
+    leftSide: {
+        flex: '0 0 50%',
         position: 'relative',
-        listStyle: 'none',
-        padding: 0,
-        margin: 0,
-        width: 380,
-        '& h6': {
-            display: 'flex',
-            alignItems: 'center',
-            color:' white',
-            fontWeight: 700}
     },
-    colorSideListInfoIcon: {
-        fontSize: 34,
-        marginRight: 15
-    },
-    colorSideListInfoItem: {
-        marginBottom: 15
-    },
-    colorSideLogoBackground: {
+    leftSideBigIcon: {
         position: 'absolute',
-        left: '50%',
-        top: '58%',
-        transform: 'translate(-50%, -50%)',
-        width: '150%',
-        height: '150%'
+        left: 0,
+        top: 0,
+        transform: 'translate(-14%, -14%)',
+        width: '197%',
+        height: '197%',
+        color: 'aqua'
     },
+    // leftSideListInfo: {
+    //     position: 'relative',
+    //     listStyle: 'none',
+    //     padding: 0,
+    //     margin: 0,
+    //     maxWidth: 380,
+    //     '& h6': {
+    //         display: 'flex',
+    //         alignItems: 'center',
+    //         color: 'white',
+    //         fontWeight: 700,
+    //         fontSize: 20,
+    //     },
+    // },
+    // leftSideListInfoItem: {
+    //     marginBottom: 40,
+    // },
+    // leftSideListInfoIcon: {
+    //     fontSize: 32,
+    //     marginRight: 15,
+    // },
     loginSide: {
+        position: 'relative',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         flex: '0 0 50%',
-
     },
-    loginSideLogo: {
-        fontSize: 100,
-        color: 'red'
+    loginSideTwitterIcon: {
+        fontSize: 45,
     },
     loginSideWrapper: {
-        width: 380
+        maxWidth: 380,
     },
     loginSideTitle: {
-        fontWeight: 800,
+        fontWeight: 700,
         fontSize: 32,
-        marginBottom: 45,
-        marginTop: 20
-    }
-}))
+        marginBottom: 60,
+        marginTop: 20,
+    },
+    loginSideField: {
+        marginBottom: 18,
+    },
+    registerField: {
+        marginBottom: theme.spacing(5),
+    },
+    loginFormControl: {
+        marginBottom: theme.spacing(2),
+    },
+}));
 
-const SingIn = () => {
-    const classes = useStyles()
+export const SignIn: React.FC = (): React.ReactElement => {
+    const classes = useStylesSignIn();
+    //установка видимости модального окна
+    const [visibleModal, setVisibleModal] = React.useState<'signIn' | 'signUp'>();
+    //для входа
+    const handleClickOpenSignIn = (): void => {
+        setVisibleModal('signIn');
+    };
+    //для регистрации
+    const handleClickOpenSignUp = (): void => {
+        setVisibleModal('signUp');
+    };
+    //для закрытия
+    const handleCloseModal = (): void => {
+        setVisibleModal(undefined);
+    };
+
     return (
         <div className={classes.wrapper}>
-            {/*блок "рекламка"*/}
-            <section className={classes.colorSide}>
-                <ColorLensIcon className={classes.colorSideLogoBackground}/>
-                <ul className={classes.colorSideListInfo}>
-                    <li className={classes.colorSideListInfoItem}><Typography variant='h6'><SearchIcon className={classes.colorSideListInfoIcon}/>Читайте о том, что интересно</Typography></li>
-                    <li className={classes.colorSideListInfoItem}><Typography variant='h6'><PeopleOutlineIcon className={classes.colorSideListInfoIcon}/> Узнайте о чем говорят</Typography></li>
-                    <li className={classes.colorSideListInfoItem}><Typography variant='h6'><ChatBubbleOutlineOutlinedIcon className={classes.colorSideListInfoIcon}/> Делитесь новостями</Typography></li>
-                </ul>
+            <section className={classes.leftSide}>
+                <ColorLensIcon color="primary" className={classes.leftSideBigIcon}/>
             </section>
-            {/*блок логинизации*/}
             <section className={classes.loginSide}>
                 <div className={classes.loginSideWrapper}>
-                    <ColorLensIcon className={classes.loginSideLogo}/>
-                    <Typography className={classes.loginSideTitle} gutterBottom variant='h4'>Узнайте все прямо сейчас</Typography>
-                    <Typography><b>Присоединяйтесь к нам</b></Typography>
+                    <ColorLensIcon color="primary" className={classes.loginSideTwitterIcon}/>
+                    <Typography className={classes.loginSideTitle} gutterBottom variant="h4">
+                        Услышал от деда, мамы, дяди, тети...
+                    </Typography>
+                    <Typography>
+                        <b>Не храни это в себе, поделись!</b>
+                    </Typography>
                     <br/>
-                    <Button style={{marginBottom: 20}} variant={'contained'} color={'primary'} fullWidth>Зарегистрироваться</Button>
-                    <Button variant={'outlined'} color={'primary'} fullWidth>Войти</Button>
+                    <Button onClick={handleClickOpenSignUp} style={{marginBottom: 20}}
+                            variant="contained" color="primary" fullWidth>
+                        Зарегистрироваться</Button>
+                    <Button onClick={handleClickOpenSignIn} variant="outlined"
+                            color="primary" fullWidth>
+                        Войти</Button>
+                    <ModalBlock visible={visibleModal === 'signIn'} onClose={handleCloseModal}
+                        classes={classes} title="Войти в аккаунт">
+                        <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
+                            <FormGroup aria-label="position" row>
+                                <TextField className={classes.loginSideField} autoFocus id="email"
+                                    label="E-Mail" InputLabelProps={{ shrink: true }} variant="filled"
+                                    type="email" fullWidth/>
+                                <TextField className={classes.loginSideField} autoFocus id="password"
+                                    label="Пароль" InputLabelProps={{ shrink: true }} variant="filled"
+                                    type="password" fullWidth/>
+                                <Button onClick={handleCloseModal} variant="contained"
+                                        color="primary" fullWidth>Войти</Button>
+                            </FormGroup>
+                        </FormControl>
+                    </ModalBlock>
+                    <ModalBlock visible={visibleModal === 'signUp'} onClose={handleCloseModal}
+                                classes={classes} title="Создайте учетную запись">
+                        <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
+                            <FormGroup aria-label="position" row>
+                                <TextField className={classes.registerField} autoFocus
+                                    id="name" label="Имя" InputLabelProps={{ shrink: true }}
+                                    variant="filled" type="name" fullWidth />
+                                <TextField className={classes.registerField} autoFocus
+                                    id="email" label="E-Mail" InputLabelProps={{ shrink: true }}
+                                    variant="filled" type="email" fullWidth />
+                                <TextField className={classes.registerField} autoFocus
+                                    id="password" label="Пароль" InputLabelProps={{ shrink: true }}
+                                    variant="filled" type="password" fullWidth />
+                                <Button variant="contained" color="primary" fullWidth>
+                                    Далее
+                                </Button>
+                            </FormGroup>
+                        </FormControl>
+                    </ModalBlock>
                 </div>
             </section>
         </div>
-    )
-}
-export default SingIn
+    );
+};
+export default SignIn
--- dolly/client/src/theme.tsx
+++ dolly/client/src/theme.tsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
-import {createMuiTheme} from "@material-ui/core/styles/";
-import {red} from '@material-ui/core/colors'
-
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
 
 export const theme = createMuiTheme({
     typography: {
@@ -13,25 +12,31 @@ export const theme = createMuiTheme({
             'Roboto',
             'Ubuntu',
             'Helvetica Neue',
-            'sans-serif'
-
-        ]
+            'sans-serif',
+        ],
     },
     palette: {
         primary: {
             main: 'rgb(29, 161, 242)',
             dark: 'rgb(26, 145, 218)',
-            contrastText: '#fff'
+            contrastText: '#fff',
         },
         secondary: {
-            main: 'rgb(26, 145, 218)'
+            main: 'rgb(26, 145, 218)',
         },
         error: {
-            main: red.A400
+            main: red.A400,
         },
         background: {
-            default: '#fff'
-        }
+            default: '#fff',
+        },
+        text: {
+            primary: '#14171a',
+        },
+        action: {
+            disabledBackground: 'rgb(153 216 255)',
+            disabled: '#fff',
+        },
     },
     shadows: [],
     overrides: {
@@ -41,31 +46,57 @@ export const theme = createMuiTheme({
                 textTransform: 'none',
                 fontSize: 16,
                 height: 40,
-                fontWeight: 900
+                fontWeight: 700,
             },
             textPrimary: {
                 paddingLeft: 20,
-                paddingRight: 20
+                paddingRight: 20,
             },
             outlinedPrimary: {
-                borderColor: 'rgb(29, 161, 243)'
+                borderColor: 'rgb(29, 161, 243)',
             },
-            MuiFilledInput: {
-                root: {
-                    borderBottom: '1px solid rgb(204, 214, 221',
-                    marginBottom: 10,
-                    padding: '10px 15px',
-                    '& h2': {
-                        display: 'flex',
-                        alignItems: 'center',
-                        fontWeight: 800
-                    },
-                    '& button': {
-                        padding: 8,
-                        marginRight: 20
-                    }
-                }
-            }
-        }
-    }
+        },
+        MuiFilledInput: {
+            underline: {
+                '&:after': {
+                    borderBottomWidth: '2px',
+                },
+                '&:before': {
+                    borderColor: '#000',
+                    borderBottomWidth: '2px',
+                },
+            },
+            input: {
+                backgroundColor: 'rgb(245, 248, 250)',
+            },
+        },
+        MuiDialog: {
+            paper: {
+                borderRadius: 15,
+            },
+        },
+        MuiDialogActions: {
+            root: {
+                marginBottom: 8,
+            },
+        },
+        MuiDialogTitle: {
+            root: {
+                borderBottom: '1px solid rgb(204, 214, 221)',
+                marginBottom: 10,
+                padding: '10px 15px',
+                '& h2': {
+                    display: 'flex',
+                    alignItems: 'center',
+                    fontWeight: 800,
+                },
+                '& button': {
+                    padding: 8,
+                    marginRight: 20,
+                },
+            },
+        },
+    },
 });
+
+export default theme;
